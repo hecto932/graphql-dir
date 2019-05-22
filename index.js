53ec82fe6dea1756bbd9c362c8f452ad32e46e7f
Hector Flores <hflores@nearsoft.comm>
2019-05-22 16:26:32 -0700

    Arguments

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const gqlMiddleware = require('express-graphql')
-const { readFileSync } = require('fs')
-const { join } = require('path')
-const resolvers = require('./lib/resolvers');
+const { makeExecutableSchema } = require("graphql-tools");
+const express = require("express");
+const gqlMiddleware = require("express-graphql");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const resolvers = require("./lib/resolvers");
 
-const port = process.env.PORT || 3000
-const app = express()
-
-const { buildSchema } = require('graphql')
+const port = process.env.PORT || 3000;
+const app = express();
 
 // Defining Schema
-const schema = buildSchema(readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8'));
+const typeDefs = readFileSync(
+  join(__dirname, "lib", "schema.graphql"),
+  "utf-8"
+);
 
-app.use('/api', gqlMiddleware({
-  schema: schema,
-  rootValue: resolvers,
-  graphiql: true
-}))
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+app.use(
+  "/api",
+  gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
+  })
+);
 
 app.listen(port, () => {
-  console.log(`GraphQL server listening on port http://localhost:${port}/`)
-})
+  console.log(`GraphQL server listening on port http://localhost:${port}/`);
+});
